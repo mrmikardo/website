81e9c07f692acb63cef5e811a21a845c09b5de14
Jack Westmore <jackwestmore@gmail.com>
2023-10-27 16:35:16 +0100

    use NuxtLink instead of anchor

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
     <section class="w-1/2">
       <h1 class="fancy-header">Poetry, engineering & creative design.</h1>
-      <p>My name is Jack. I'm a full-stack designer, software engineer, and systems thinker. I believe software can and should be elegant, cruft-free, and a delight to use.</p><br>
-      <p>At the moment I'm available for both full-time and freelance work. If you'd like to reach out, don't hesitate to <a href="/contact">get in touch</a>.</p>
+      <p>My name is Jack. I'm a full-stack engineer, poet, and systems thinker. I believe software can and should be elegant, cruft-free, and a delight to use.</p><br>
+      <p>At the moment I'm available for both full-time and freelance work. If you'd like to reach out, don't hesitate to <NuxtLink class="underline" to="/contact">get in touch</NuxtLink>.</p>
     </section>
   </template>
   
