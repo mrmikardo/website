410c1838482b4c95d4c00e03b72434bca65be517
Jack Westmore <jackwestmore@gmail.com>
2023-10-27 23:03:22 +0100

    Navbar: - redundant style tag

--- components/navbar/Navbar.vue
+++ components/navbar/Navbar.vue
@@ -8,20 +8,3 @@ import Anchor from "../shared/Anchor.vue";
     <Anchor href="/contact" text="contact" />
   </div>
 </template>
-
-<style scoped>
-a {
-  text-decoration: none;
-  background-image: linear-gradient(#1e4c7c, #1e4c7c);
-  background-size: 0% 0.1em;
-  background-position-y: 100%;
-  background-position-x: 0%;
-  background-repeat: no-repeat;
-  transition: background-size 0.2s ease-in-out;
-}
-a:hover,
-a:focus,
-a:active {
-  background-size: 100% 0.1em;
-}
-</style>
